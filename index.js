a048ad866a33be91a24e462489a66c58e050e0e0
simon07gd <sgrzywna@griddynamics.com>
2025-09-03 18:06:59 +0200

    Add url encoded

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
