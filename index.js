c13ee107149b233b944f391f79e2d2225932539e
simon07gd <sgrzywna@griddynamics.com>
2025-09-04 18:50:39 +0200

    Add dirname filename consts

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import { Database } from 'sqlite-async';
 import 'dotenv/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express(); 
 
 app.use(cors())
